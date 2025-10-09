4d15a6dd134df6ad3dc2a65cb728dce971f822a6
HECTOR-BENITEZ2634 <146160444+HECTOR-BENITEZ2634@users.noreply.github.com>
2025-10-08 20:02:18 -0600

    Update ejemplo.py
    modifiqué saludar para mostrar la hora

--- código/ejemplo.py
+++ código/ejemplo.py
@@ -1,5 +1,10 @@
+# Importamos datetime para obtener la hora actual
+from datetime import datetime 
+
 def saludar(nombre):
-    return f"Hola {nombre}, este es un ejemplo de función en Python."
+    # Obtenemos la hora actual 
+    hora = datetime.now().strftime("%H:%M")  
+    return f"Hola {nombre}, son las {hora}. Bienvenido al ejemplo de Python"
 
 if __name__ == "__main__":
-    print(saludar("Equipo"))
+    print(saludar("Equipo verde"))
